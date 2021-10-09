52f32b1158bdc57692a98f5a92b742d33989259f
x04000 <78761999+x04000@users.noreply.github.com>
2021-10-09 21:23:30 +0200

    Add files via upload

---
+++ ToHash.py
@@ -0,0 +1,14 @@
+import hashlib
+import random
+
+guess_password = input("Text: ")
+guess_passwordf = str(guess_password)
+
+H = hashlib.md5(str(guess_passwordf).encode('utf-8'))
+
+m = hashlib.md5()
+line = H.hexdigest()
+m.update(str(line).encode('utf-8'))
+word_hash = m.hexdigest()
+
+print(word_hash)
